f207474220555c8d991852e669da1d2144ad024b
WaveDR <104482152+WaveDR@users.noreply.github.com>
2023-05-05 13:33:06 +0900

    ScoreItem Swich 구현
    플레이어가 레몬을 획득 시 주변 장애물 오브젝트들을 추가점수를 줄 수 있는 용과 아이템으로 변경하는 로직을 구현했습니다.

--- Assets/3.Script/ILemon.cs
+++ Assets/3.Script/ILemon.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class ILemon : MonoBehaviour
 {
-    public enum ItemType { Lemon,DragonFruit}
+    public enum ItemType { Lemon, DragonFruit}
     public ItemType type;
+
+    SphereCollider sphere;
+
+    public bool isRoot = false;
+
+    private void OnEnable()
+    {
+        isRoot = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Pipe") && type.Equals(ItemType.Lemon))
+        if (other.CompareTag("Pipe") && type.Equals(ItemType.Lemon) && isRoot)
         {
             ObjectChange box = other.GetComponent<ObjectChange>();
+            box.isChange = true;
+            box.ChangeObj();
+            //gameObject.SetActive(false);
         }
 
+        //용과로 접촉 시
         if (other.CompareTag("Player") && type.Equals(ItemType.DragonFruit))
         {
-
-            
             gameObject.SetActive(false);
         }
     }
