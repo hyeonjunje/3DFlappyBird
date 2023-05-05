40af538da0cf3bf1ee171240630b64f9f8b0e17c
juhye1 <wnzk01@gmail.com>
2023-05-05 18:46:37 +0900

    버그수정

--- Assets/Wait.cs
+++ Assets/Wait.cs
@@ -7,38 +7,24 @@ public class Wait : MonoBehaviour
 {
     public GameObject WaitUI;
     public Text countDown;
-    private float timer = 3;
 
-    private void Awake()
-    {
-        countDown.text = timer.ToString();
-    }
 
     private void Start()
     {
         StartCoroutine(oo());
-    }
-    void Update()
-    {
-/*            timer -= Time.unscaledDeltaTime;
-        if (timer > 0)
-        {
-            countDown.text = Mathf.Round(timer).ToString();
-        }
-        else if (timer <= 0)
-        {
-            WaitUI.SetActive(false);
-        }*/
-
     }
 
     IEnumerator oo()
     {
         WaitUI.SetActive(true);
         Time.timeScale = 0.0f;
-        yield return new WaitForSecondsRealtime(3f);
-        WaitUI.SetActive(false);
+        for(int i=3; i>0; i--)
+        {
+            countDown.text = i.ToString();
+            yield return new WaitForSecondsRealtime(1f);
+        } 
         Time.timeScale = 1.0f;
+        WaitUI.SetActive(false);
     }
 
 
