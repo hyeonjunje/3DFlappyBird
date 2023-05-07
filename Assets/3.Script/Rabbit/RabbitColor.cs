2823a0ecf6d2f2a4dc6d7735665fc8917c0cfd9c
juhye1 <127804324+juhye1@users.noreply.github.com>
2023-05-07 11:54:27 +0900

    끝

--- Assets/3.Script/Rabbit/RabbitColor.cs
+++ Assets/3.Script/Rabbit/RabbitColor.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class RabbitColor : MonoBehaviour
 {
     public SkinnedMeshRenderer rend;
+    Animator ani;
 
     private void Awake()
     {
         rend = GetComponentInChildren<SkinnedMeshRenderer>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        ani.SetTrigger("IdleC");
     }
 
 }
